47f35a1a4d9ac4722fd2739d52fe53cb2b5f28c8
Pavel Gural <pgural01@gmail.com>
2024-05-08 15:47:27 +0300

    [AWS LAMDBA] Add comments to app.py

--- app.py
+++ app.py
@@ -1,14 +1,17 @@
 import time
-
 import yaml
-
 from lib import mod3
 
 
 # Code here will change quite frequently
 def main(event, context):
+    """
+    Main function to be executed when the Lambda is invoked.
+
+    :param event: AWS Lambda uses this parameter to pass in event data to the handler.
+    :param context: AWS Lambda uses this parameter to provide runtime information to your handler.
+    """
     print(f"Event: {event}, Context: {context}")
     print(f"Running with yaml version: [{yaml.__version__}]")
     if mod3(int(time.time())):
         raise Exception("Random exception 1/3 of the time")
-
